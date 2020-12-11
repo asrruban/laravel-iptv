7ccd2fa4dd150d1157e112b79642cddbb845640b
asrruban <asr.ruban@yahoo.com>
2020-12-11 18:41:33 +0600

    Add files via upload

---
+++ src/views/channels_list_api.blade.php
@@ -0,0 +1,9 @@
+#EXTM3U
+
+@foreach($list as $Channel)
+
+#EXTINF:-1 type="stream"  @if($Channel->radio) radio=true @else tvg-id="{{$Channel->number}}" tvg-name="{{$Channel->name}}" @endif tvg-logo="{{ url($Channel->logo) }}" group-title="{{ $Channel->group->name }}",{{$Channel->name}}
+{{$Channel->url_stream}}
+@endforeach
+
+
